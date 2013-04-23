20b55cf82c3a3f31bc96fb4528d116101a60c811
Janna <Janna@10.20.209.143>
2013-04-23 10:56:41 +0300

    DeviceContainer in the Service created

--- Group19HomeControl/src/fi/oulu/tol/group19project/HomeControlService.java
+++ Group19HomeControl/src/fi/oulu/tol/group19project/HomeControlService.java
@@ -1,14 +1,25 @@
 package fi.oulu.tol.group19project;
 
+
+import fi.oulu.tol.group19project.model.AbstractDevice.Type;
+import fi.oulu.tol.group19project.model.ConcreteDevice;
+import fi.oulu.tol.group19project.model.DeviceContainer;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 
 public class HomeControlService extends Service {
 
 
-private final HomeControlBinder binder = new HomeControlBinder();
+	private final HomeControlBinder binder = new HomeControlBinder();
+	private DeviceContainer devices = new DeviceContainer(null, "container-1", "Test Container", null, null);
+
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		debugInitialize();
+	}
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
@@ -20,10 +31,17 @@ private final HomeControlBinder binder = new HomeControlBinder();
 			return HomeControlService.this;
 		}
 	}
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return binder;
 	}
 
+	public void debugInitialize () {
+	devices.add(new ConcreteDevice(null, Type.SENSOR, "id-for-sersor-1", "Light sensor", "Outside in back yard", null, 800.0, 10.0, 4000.0, "lumen"));
+	DeviceContainer cont = new DeviceContainer(null, "container-2", "Restroom", "Loo for poo", null);
+	cont.add(new ConcreteDevice(null, Type.ACTUATOR, "id-for-actuator-1", "Door lock", "Loo door", null, 0.0, 0.0, 1.0, null));
+	devices.add(cont);
+
+	}
 }
